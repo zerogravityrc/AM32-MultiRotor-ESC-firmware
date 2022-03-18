5583ee80efc3d2fe725908454c060701a17ec05e
zerogravityrc <97279012+zerogravityrc@users.noreply.github.com>
2022-03-18 02:07:14 -0700

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -138,23 +138,23 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 76
 char dir_reversed = 0;
-char comp_pwm = 1;
-char VARIABLE_PWM = 1;
+char comp_pwm = 0;
+char VARIABLE_PWM = 0;
 char bi_direction = 0;
 char stuck_rotor_protection = 0;	// Turn off for Crawlers
-char brake_on_stop = 1;
+char brake_on_stop = 0;
 char stall_protection = 0;
 char use_sin_start = 0;
 char THIRTY_TWO_MS_TLM = 0;
 
 char advance_level = 2;			// 7.5 degree increments 0 , 7.5, 15, 22.5)
-uint16_t motor_kv = 2000;
+uint16_t motor_kv = 1500;
 char motor_poles = 14;
 //add Startup Power
 //Add PWM Frequency
 //Add Beep Volume
 char drag_brake_strength = 10;		// Drag Brake Power
-char sine_mode_changeover_thottle_level = 5;	// Sine Startup Range
+char sine_mode_changeover_thottle_level = 6;	// Sine Startup Range
 
 char USE_HALL_SENSOR = 0;
 
@@ -166,7 +166,7 @@ uint8_t servo_dead_band = 100;
 
 //========================= Battery Cuttoff Settings ========================
 char LOW_VOLTAGE_CUTOFF = 0;		// Turn Low Voltage CUTOFF on or off
-uint16_t low_cell_volt_cutoff = 330;	// 3.3volts per cell
+uint16_t low_cell_volt_cutoff = 350;	// 3.3volts per cell
 
 //Add Car/basher mode
 
